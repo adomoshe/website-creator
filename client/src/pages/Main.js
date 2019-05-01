197338dc4cc4350ee9b6cc35365a0dd0c1461c27
Ado-Work <ado.astpos@gmail.com>
2019-05-01 11:21:40 -0700

    Deleted unnecessary components

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import './Main.css';
 
-import MainCarousel from '../components/Carousel';
-import About from '../components/About';
-import Skills from '../components/Skills';
-import Projects from '../components/Projects';
-import Education from '../components/Education';
-import Experience from '../components/Experience';
 import Footer from '../components/Footer';
 
 class Main extends Component {
@@ -16,13 +10,7 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <MainCarousel />
-        <About />
-        <Skills />
-        <Projects />
-        <Education />
-        <Experience />
-        <Footer niki />
+        <Footer/>
       </div>
     );
   }
